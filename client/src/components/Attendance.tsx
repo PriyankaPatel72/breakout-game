9ca0b47d0badca790441cfb3c40ec6040e9a530d
Jason Liu <143151844+jay11256@users.noreply.github.com>
2025-04-30 17:01:21 -0400

    Complete attendance HTML

--- client/src/components/Attendance.tsx
+++ client/src/components/Attendance.tsx
@@ -11,6 +11,13 @@ const AttendanceDB = [
             1: true,
             2: false,
             3: true,
+            4: true,
+            5: true,
+            6: true,
+            7: true,
+            8: true,
+            9: false,
+            10: true,
         }
     },
     {
@@ -35,7 +42,7 @@ function Attendance(props: JSX.IntrinsicAttributes & { admin: any; }) {
 
     const navigate = useNavigate();
     const weekNumbers = Array.from(
-        new Set(AttendanceDB.flatMap(student => Object.keys(student.attendance)))
+        new Set(AttendanceDB.flatMap(student => Object.keys(student.attendance).map(Number)))
     ).sort((a, b) => Number(a) - Number(b));
 
     // Route back to home page if not admin
@@ -52,7 +59,34 @@ function Attendance(props: JSX.IntrinsicAttributes & { admin: any; }) {
             </div>
 
             <div className="main-screen">
-
+                <h1>Attendance</h1>
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                {weekNumbers.map(week => (
+                                    <th>Week {week}</th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {AttendanceDB.map(student => (
+                            <tr>
+                                <td>{student.name}</td>
+                                {weekNumbers.map(week => {
+                                const attended = student.attendance[week];
+                                    return (
+                                        <td>
+                                            {attended ? '✅' : '❌'}
+                                        </td>
+                                    );
+                                })}
+                            </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </>
     )
